c2f79285d1f772df4e6b4be12c83f81266de52d1
r23802430 <maroki.igarashi@jp.ricoh.com>
2023-02-03 14:22:22 +0900

    WyCash/WyCashTest新規作成

---
+++ WyCash/Money/Dollar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WyCash
+{
+    public class Dollar
+    {
+        public int amount = 10;
+
+        public Dollar( int amount )
+        { }
+
+        public void Times( int multiplier )
+        { }
+    }
+}
---
+++ WyCashTest/Test/MoneyTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WyCash;
+
+namespace WyCashTest
+{
+    public class MoneyTest
+    {
+        [Test]
+        public void Test01_Multiplication()
+        {
+            Dollar five = new Dollar( 5 );
+            five.Times( 2 );
+            Assert.AreEqual( 10, five.amount );
+        }
+    }
+}
