824189d87f2dbf10c0e8653e4688f565562a51bd
r23802430 <maroki.igarashi@jp.ricoh.com>
2023-02-06 15:45:13 +0900

    Francクラスを追加

---
+++ WyCash/Money/Franc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WyCash
+{
+    public class Franc
+    {
+        private int amount;
+
+        public Franc( int amount )
+        {
+            this.amount = amount;
+        }
+
+        public Franc Times( int multiplier )
+        {
+            return new Franc( this.amount * multiplier );
+        }
+
+        public bool IsEqual( object target )
+        {
+            return this.amount == ( (Franc)target ).amount;
+        }
+    }
+}
--- WyCashTest/Test/MoneyTest.cs
+++ WyCashTest/Test/MoneyTest.cs
@@ -11,7 +11,7 @@ namespace WyCashTest
     public class MoneyTest
     {
         [Test]
-        public void Test01_Multiplication()
+        public void Test01_DollarMultiplication()
         {
             Dollar five = new Dollar( 5 );
 
@@ -23,13 +23,25 @@ namespace WyCashTest
         }
 
         [Test]
-        public void Test02_Equality()
+        public void Test02_DollarEquality()
         {
             Assert.IsTrue( new Dollar( 5 ).IsEqual( new Dollar( 5 ) ) );
             Console.WriteLine( "new Dollar( 5 ).IsEqual( new Dollar( 5 ) ) = True" );
 
             Assert.IsFalse( new Dollar( 5 ).IsEqual( new Dollar( 6 ) ) );
             Console.WriteLine( "new Dollar( 5 ).IsEqual( new Dollar( 6 ) ) = False" );
+        }
+
+        [Test]
+        public void Test03_FrancMultiplication()
+        {
+            Franc five = new Franc( 5 );
+
+            Assert.IsTrue( new Franc( 10 ).IsEqual( five.Times( 2 ) ) );
+            Console.WriteLine( "new Franc( 10 ) = new Franc( 5 ).Times( 2 )" );
+
+            Assert.IsTrue( new Franc( 15 ).IsEqual( five.Times( 3 ) ) );
+            Console.WriteLine( "new Franc( 15 ) = new Franc( 5 ).Times( 3 )" );
         }
     }
 }
