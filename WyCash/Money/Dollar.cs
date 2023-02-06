ada0c9e2ae335a9f77840f585fb771a13347583f
r23802430 <maroki.igarashi@jp.ricoh.com>
2023-02-06 17:09:19 +0900

    Moneyクラスを追加してDollar/Francクラスはそれを継承

--- WyCash/Money/Dollar.cs
+++ WyCash/Money/Dollar.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WyCash
 {
-    public class Dollar
+    public class Dollar : Money
     {
         private int amount;
 
--- WyCash/Money/Franc.cs
+++ WyCash/Money/Franc.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WyCash
 {
-    public class Franc
+    public class Franc : Money
     {
         private int amount;
 
