9d69a4f858678aac483cf278e1791d48207a2d9a
r23802430 <maroki.igarashi@jp.ricoh.com>
2023-02-20 16:14:58 +0900

    [P75] Dollar/FrancメソッドでMoneyクラスのインスタンスを返すように変更

--- WyCash/Money/Money.cs
+++ WyCash/Money/Money.cs
@@ -40,12 +40,12 @@ namespace WyCash
 
         public static Money Dollar( int amount )
         {
-            return new Dollar( amount, "USD" );
+            return new Money( amount, "USD" );
         }
 
         public static Money Franc( int amount )
         {
-            return new Franc( amount, "CHF" );
+            return new Money( amount, "CHF" );
         }
     }
 }
