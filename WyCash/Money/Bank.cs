c3ab5b6f9a6baa2dba28eb88e39b13ed4b931e17
r23802430 <maroki.igarashi@jp.ricoh.com>
2023-03-07 17:25:11 +0900

    [P97] 型チェック・キャストを排除

--- WyCash/Money/Bank.cs
+++ WyCash/Money/Bank.cs
@@ -10,12 +10,7 @@ namespace WyCash
     {
         public Money Reduce( Expression source, string to )
         {
-            if( source is Money )
-            {
-                return ( (Money)source ).Reduce( to );
-            }
-            Sum sum = (Sum)source;
-            return sum.Reduce( to );
+            return source.Reduce( to );
         }
     }
 }
