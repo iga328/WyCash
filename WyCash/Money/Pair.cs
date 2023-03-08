363b1f358e480a046a8c9df1d9d1eb0674422485
r23802430 <maroki.igarashi@jp.ricoh.com>
2023-03-08 11:59:01 +0900

    [P105] Pairクラスを追加

---
+++ WyCash/Money/Pair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WyCash
+{
+    public class Pair
+    {
+        private string from;
+        private string to;
+
+        public Pair( string from, string to )
+        {
+            this.from = from;
+            this.to = to;
+        }
+    }
+}
